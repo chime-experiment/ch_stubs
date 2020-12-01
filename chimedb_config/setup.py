0a5b5c72678c9a8a03d05cbee238de466fdd6220
anjakefala <anja.kefala@gmail.com>
2020-11-30 16:48:32 -0800

    feat(chimedb_config) add stub for chimedb_config

---
+++ chimedb_config/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import setuptools
+
+setuptools.setup(
+        name="chimedb_config",
+        author_email="anja@phas.ubc.ca",
+        description="A stub module to use as a mock import of the private chimedb_config",
+        packages=["chimedb.config"],
+        url="https://github.com/chime-experiment/ch_stubs/chimedb_config"
+    )

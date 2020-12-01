1c9a776e9bcf0552b786274022cd5d255f947f7f
anjakefala <anja.kefala@gmail.com>
2020-11-30 16:45:47 -0800

    feat(chimedb_dataset): add chimedb_dataset stub

---
+++ chimedb_dataset/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import setuptools
+
+setuptools.setup(
+        name="chimedb_dataset",
+        author_email="anja@phas.ubc.ca",
+        description="A stub module to use as a mock import of the private chimedb_dataset",
+        packages=["chimedb.dataset"],
+        url="https://github.com/chime-experiment/ch_stubs/chimedb_dataset"
+        )
